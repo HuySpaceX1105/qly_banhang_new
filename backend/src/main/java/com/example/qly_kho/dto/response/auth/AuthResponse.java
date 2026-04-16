13a014037cbb590ed4fda94602293202f581172b
HuyTD <huy32675@gmail.com>
2026-04-16 15:14:23 +0700

    Delete backend/src/main/java/com/example/qly_kho/dto directory

--- backend/src/main/java/com/example/qly_kho/dto/response/auth/AuthResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.qly_kho.dto.response.auth;
-
-public record AuthResponse(
-    String accessToken,
-    String refreshToken,
-    String tokenType,
-    AuthUserResponse user //userId, username, roles, fullName, email
-) {}
--- backend/src/main/java/com/example/qly_kho/dto/response/role/RoleResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.qly_kho.dto.response.role;
-
-public record RoleResponse(
-    Long roleId,
-    String name,
-    String description
-) {}
