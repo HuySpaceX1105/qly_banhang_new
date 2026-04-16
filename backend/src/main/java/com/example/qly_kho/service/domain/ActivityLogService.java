8f9add141572790a758c9c417189aedd046d2394
HuyTD <huy32675@gmail.com>
2026-04-16 15:15:01 +0700

    Delete backend/src/main/java/com/example/qly_kho/service directory

--- backend/src/main/java/com/example/qly_kho/service/domain/ActivityLogService.java
+++
@@ -1,7 +0,0 @@
-package com.example.qly_kho.service.domain;
-
-public interface ActivityLogService {
-
-    //ghi nhật kí hoạt động của người dùng
-    void logActivity(Long userId, String action, String entityType, long entityId, String description, String ipAddress);
-}
--- backend/src/main/java/com/example/qly_kho/service/domain/PermissionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.qly_kho.service.domain;
-
-import com.example.qly_kho.entity.Permission;
-
-public interface PermissionService {
-
-    //tìm permission theo id
-    public Permission findById(Long id);
-}
